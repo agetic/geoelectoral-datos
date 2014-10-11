63a51555d1f06607643a3ead73fdf4562afba931
Sylvain Lesage <severo@rednegra.net>
2014-10-11 14:34:40 -0400

    para pruebas

--- scripts/upgrade_0.4_hacia_0.5.sql
+++ scripts/upgrade_0.4_hacia_0.5.sql
@@ -3,4 +3,4 @@
 -------------------------------------------
 
 -- Por ejemplo, para ingresar el resultado 3000000 votos para el MAS en Bolivia: SELECT f_insert_resultado_2014_plurinacional_votos(25, 1, 3000000);
-
+SELECT f_insert_resultado_2014_plurinacional_votos(25, 1, 3000000);
