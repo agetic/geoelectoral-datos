a4b999233b62f4a5fe554bd55df855251f0a3121
Sylvain Lesage <severo@rednegra.net>
2014-10-12 19:18:54 -0400

    Versión 0.5: adición de 2 nuevos municipios

---
+++ scripts/upgrade_0.6_hacia_0.7.sql
@@ -0,0 +1,6 @@
+-------------------------------------------
+-- Adición resultados de las elecciones 2014
+-------------------------------------------
+
+-- Por ejemplo, para ingresar el resultado 3000000 votos para el MAS en Bolivia: SELECT f_insert_resultado_2014_plurinacional_votos(25, 1, 3000000);
+SELECT f_insert_resultado_2014_plurinacional_votos(25, 1, 3000000);
